5977aecb8a2026a9579f9646bbcc230534d9565f
inadequateDr <dreamerinadequate@gmail.com>
2023-12-10 02:09:10 +0200

    all
    all

--- js/task-2.js
+++ js/task-2.js
@@ -1,37 +1,31 @@
-const getUsersWithFriend = (users, friendName) =>
-  users.filter(user => user.friends.includes(friendName));
+class Storage {
+  #items;
 
-const allUsers = [
-  {
-    name: 'Moore Hensley',
-    friends: ['Sharron Pace'],
-  },
-  {
-    name: 'Sharlene Bush',
-    friends: ['Briana Decker', 'Sharron Pace'],
-  },
-  {
-    name: 'Ross Vazquez',
-    friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
-  },
-  {
-    name: 'Elma Head',
-    friends: ['Goldie Gentry', 'Aisha Tran'],
-  },
-  {
-    name: 'Carey Barr',
-    friends: ['Jordan Sampson', 'Eddie Strong'],
-  },
-  {
-    name: 'Blackburn Dotson',
-    friends: ['Jacklyn Lucas', 'Linda Chapman'],
-  },
-  {
-    name: 'Sheree Anthony',
-    friends: ['Goldie Gentry', 'Briana Decker'],
-  },
-];
+  constructor(initialItems) {
+    this.#items = initialItems;
+  }
 
-console.log(getUsersWithFriend(allUsers, 'Briana Decker'));
-console.log(getUsersWithFriend(allUsers, 'Goldie Gentry'));
-console.log(getUsersWithFriend(allUsers, 'Adrian Cross'));
+  getItems() {
+    return this.#items;
+  }
+
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    const indexToRemove = this.#items.indexOf(itemToRemove);
+    if (indexToRemove !== -1) {
+      this.#items.splice(indexToRemove, 1);
+    }
+  }
+}
+
+const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
+console.log(storage.getItems());
+
+storage.addItem('Droid');
+console.log(storage.getItems());
+
+storage.removeItem('Prolonger');
+console.log(storage.getItems());
